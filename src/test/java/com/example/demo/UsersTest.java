bb1abb70e7f90eea6af52303d939034e94f8a03c
Yuriy Shpyluk <yshpyluk@tidemark.com>
2017-06-11 23:32:25 +0300

    clean up

--- src/test/java/com/example/demo/UsersTest.java
+++ src/test/java/com/example/demo/UsersTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,10 +14,9 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
-//@ContextConfiguration
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class UsersTest {
 	@Autowired
 	private MockMvc mockMvc;
@@ -45,7 +43,7 @@ public class UsersTest {
 				.andExpect(MockMvcResultMatchers.view().name("users"));
 	}
 
-	//test /users/{profile} && /users/api/profile endpoint
+	//test /users/{profile} && /users/api/{profile} endpoint
 	@Test
 	public void testContentOfProfileApiResponse() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/users/api/yshpyluk"))
